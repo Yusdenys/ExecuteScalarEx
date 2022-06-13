65694554ae180a21fa1a0c6a323c3862de55396a
Yusdenys Perez <yudemendoza@gmail.com>
2022-06-13 16:53:41 -0400

    C# Dapper delete row

---
+++ DeleteRow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+
+
+namespace ExecuteScalarEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = @"Data Source=LENOVODEV\LENOVOSERVER;Initial Catalog=dbdapper;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using var con = new SqlConnection(cs);
+            con.Open();
+
+            var delRows = con.Execute(@"DELETE FROM [cars] WHERE Id = @Id", new { Id = 1 });
+
+            if (delRows > 0)
+            {
+                Console.WriteLine("car deleted");
+            }
+        }
+    }
+}
