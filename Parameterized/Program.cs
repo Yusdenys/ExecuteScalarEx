b63c70a005c04b4df59d70fdeddc43054fa9b11a
Yusdenys Perez <yudemendoza@gmail.com>
2022-06-13 16:28:58 -0400

    C# Dapper parameterized query

---
+++ Parameterized/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+
+
+namespace Parameterized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = @"Data Source=LENOVODEV\LENOVOSERVER;Initial Catalog=dbdapper;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using var con = new SqlConnection(cs);
+            con.Open();
+
+            var car = con.QueryFirst<Car>("SELECT * FROM cars WHERE id=@id", new { id = 3 });
+
+            Console.WriteLine(car);
+        }
+    }
+}
